4f43efe5c4e96616ab5761655d5265b33a51bde8
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-02-19 00:06:35 -0500

    fixed clicking on image didn't redirect

--- minimal-portfolio/src/data/portfolio.js
+++ minimal-portfolio/src/data/portfolio.js
@@ -3,18 +3,18 @@ export default [
         title:'Map Dashboard',
         imgUrl: '/assets/mapdashpic.jpeg',
         stack: ['React.js','React Mapbox','HTML/CSS'],
-        link:'github.com/ahmkhn',
+        link:'https://github.com/ahmkhn',
     },
     {
         title:'Massachusetts Federal Payroll Searcher',
         imgUrl: '/assets/payroll-search-pic.png',
         stack: ['Java','JavaFX','Gradle'],
-        link:'github.com/ahmkhn',
+        link:'https:github.com/ahmkhn',
     },
     {
         title:'ToDo List',
         imgUrl: '/assets/todolist.png',
         stack: ['React.js','HTML/CSS','Git'],
-        link:'github.com/ahmkhn',
+        link:'https:github.com/ahmkhn',
     },
 ]
